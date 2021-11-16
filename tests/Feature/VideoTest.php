888ddce602fba920e12aa5fcd15471a72c20ec0a
sergicasado <sergicasado1@iesebre.com>
2021-11-16 17:53:22 +0100

    wip

--- tests/Feature/VideoTest.php
+++ tests/Feature/VideoTest.php
@@ -8,36 +8,40 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
+
 class VideoTest extends TestCase
 {
-    use RefreshDatabase;    //ESTAT PRECONEGUT -> ZERO STATE
+    use RefreshDatabase;        // ESTAT PRECONEGUT -> ZERO STATE
+
     /**
      * @test
      */
     public function users_can_view_videos()
     {
-        //FASE 1 -> Preparació -> Prepare
-        //WISHFUL PROGRAMMING -> API
         $video = Video::create([
-                'title' => 'Ubuntu 101',
-                'description' => '# Here description',
-                'url' => 'https://youtu.be/w8j07_DBl_I',
-                'published_at' => Carbon::parse('December 13, 2020 8:00pm'),
-                'previous' => null,
-                'next' => null,
-                'series_id' => 1
+            'title' => 'Ubuntu 101',
+            'description' => '# Here description',
+            'url' => 'https://youtu.be/w8j07_DBl_I',
+            'published_at' => Carbon::parse('December 13, 2020 8:00pm'),
+            'previous' => null,
+            'next' => null,
+            'series_id' => 1
         ]);
 
+        $response = $this->get('/videos/' . $video->id); // SLUGS -> SEO -> TODO
 
-        //FASE 2 -> Execució -> Executa el codi a provar
-        //Laravel HTTP TESTS ->
-//        dd('/videos/1' . $video->id);
-        $response = $this->get('/videos/1' . $video->id); // SLUGS -> SEO -> TODO
-
-        //FASE 3 -> Assertions -> comprovacions
         $response->assertStatus(200);
         $response->assertSee('Ubuntu 101');
         $response->assertSee('Here description');
         $response->assertSee('December 13');
+    }
+
+    /**
+     * @test
+     */
+    public function users_cannot_view_not_existing_videos()
+    {
+        $response = $this->get('/videos/999');
+        $response->assertStatus(404);
     }
 }
