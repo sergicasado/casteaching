07ee59c6333769959c3287bbe9f7e6b1395f7574
sergicasado <sergicasado1@iesebre.com>
2021-12-09 16:10:03 +0100

    wip

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Video;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,11 +20,11 @@ Route::get('/', function () {
 
 Route::get('/videos/1', function () {
 //    return 'Ubuntu 101 | Here description | December 13';
-
-    $video = new stdClass();
-    $video->title = 'Ubuntu 101';
-    $video->description = 'Here description';
-    $video->published_at = 'December 13';
+    $video = Video::find(1);
+//    $video = new stdClass();
+//    $video->title = 'Ubuntu 101';
+//    $video->description = 'Here description';
+//    $video->published_at = 'December 13';
     return view('videos.show',[
         'video' => $video
     ]); // CRUD -> RETRIEVE -> nomes un video
