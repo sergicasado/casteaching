ad7e4f751b5f329ee04f21f58dfb6da8203a3b40
sergicasado <sergicasado1@iesebre.com>
2022-01-20 15:44:30 +0100

    Error 404

--- tests/Feature/Videos/VideoTest.php
+++ tests/Feature/Videos/VideoTest.php
@@ -9,6 +9,10 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 
+/**
+ * @covers \App\Http\Controllers\VideosController
+ */
+
 class VideoTest extends TestCase
 {
     use RefreshDatabase;
@@ -41,6 +45,15 @@ class VideoTest extends TestCase
         $response->assertSee('Ubuntu 101');
         $response->assertSee('Here description');
         $response->assertSee('December 13');
+    }
+
+    /**
+     * @test
+     */
+    public function users_cannot_view_not_existing_videos(){
+        $response = $this->get('/videos/999');
+        $response->assertStatus(404);
+
     }
 
 }
