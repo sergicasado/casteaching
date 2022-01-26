ddccd7d728f0bd1afd9415cab13eb76b138f76bb
sergicasado <sergicasado1@iesebre.com>
2022-01-26 17:55:22 +0100

    wip

--- resources/views/videos/show.blade.php
+++ resources/views/videos/show.blade.php
@@ -1,5 +1,6 @@
 <x-casteaching-layout>
-    <div class="flex flex-col h-screen scape-x-4 space-y-4 lg:space-x-4 lg:space-y-4">
+    <div class="flex flex-col h-screen space-x-4 space-y-4 lg:space-x-6 lg:space-y-4 xl:space-x-15 xl:space-y-5
+2xl:space-x-20 2xl:space-y-10">
         <iframe
             class="md:p-3 lg:p-5 xl:px-10 xl:py-5 2xl:px-20 2xl:py-10 h-4/5"
             height="315"
@@ -8,8 +9,10 @@
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen></iframe>
 
-        <div>
-           <h2>{{$video->title}}</h2>
+        <div class="prose">
+            <h2>
+                {{$video->title}}
+            </h2>
         </div>
         <div>
             {{$video->description}}
