d92374e524f10e89a2d4bc52919228ac2fbd5500
sergicasado <sergicasado1@iesebre.com>
2022-01-26 16:44:09 +0100

    wip

--- resources/views/videos/show.blade.php
+++ resources/views/videos/show.blade.php
@@ -1,10 +1,19 @@
 <x-casteaching-layout>
-    <div>
-        <p>{{$video->title}}</p>
+    <div class="flex flex-col">
+        <iframe
+            class="sm:px-3"
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/btGr3mPK1dU"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen></iframe>
 
-        <ul>
-            <li>Description: {{$video->description}}</li>
-            <li>Data: {{$video->published_at->format('F d') }}</li>
-        </ul>
+        <div>
+           <h2>{{$video->title}}</h2>
+        </div>
+        <div>
+            {{$video->description}}
+        </div>
     </div>
 </x-casteaching-layout>
