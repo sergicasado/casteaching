492c9063ad988f5e26c4c1b8d844d1cb58c5870a
sergicasado <sergicasado1@iesebre.com>
2022-01-27 16:21:16 +0100

    video 112

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -2,14 +2,20 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Tests\Unit\VideoTest;
 
 class Video extends Model
 {
     use HasFactory;
+    public static function testedBy()
+    {
+        return VideoTest::class;
+    }
+    protected $guarded = [];
 
-      protected $guarded = [];
 //    protected $fillable = ['title','description'];
 
       protected $dates = ['published_at'];
@@ -17,4 +23,22 @@ class Video extends Model
 //            'published' => 'datetime:Y-m-d'
 //      ];
 
+
+    //formatted_published_at accesor
+    public function getFormattedPublishedAtAttribute()
+    {
+        if(!$this->published_at) return '';
+        $locale_date = $this->published_at->locale(config('app.locale'));
+        return $locale_date->day . ' de ' . $locale_date->monthName . ' de ' . $locale_date->year;
+    }
+
+    public function getFormattedForHumansPublishedAtAttribute()
+    {
+        return optional($this->published_at)->diffForHumans(Carbon::now());
+    }
+
+    public function getPublishedAtTimestampAttribute()
+    {
+        return optional($this->published_at)->timestamp;
+    }
 }
--- tests/Unit/VideoTest.php
+++ tests/Unit/VideoTest.php
@@ -4,15 +4,21 @@ namespace Tests\Unit;
 
 use App\Models\Video;
 use Carbon\Carbon;
-use PHPUnit\Framework\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
 
+/**
+ * @covers Video::class
+ */
 class VideoTest extends TestCase
 {
-    /** @test **/
-    public function cant_get_formatted_date()
+    use RefreshDatabase;
+
+    /** @test */
+    public function can_get_formatted_published_at_date()
     {
-        //1 Preparacio
-        //TODO code smell
+        // 1 Preparació
+        // TODO CODE SMELL
         $video = Video::create([
             'title' => 'Ubuntu 101',
             'description' => '# Here description',
@@ -23,10 +29,32 @@ class VideoTest extends TestCase
             'series_id' => 1
         ]);
 
-        //2 Execució WISHFUL PROGRAMMING
+        // 2 Execució WISHFUL PROGRAMMING
         $dateToTest = $video->formatted_published_at;
 
-        //3 comprovació
-        $this->assertEquals($dateToTest,'13 de desembre de 2020');
+        // 3 comprovació / assert
+        $this->assertEquals($dateToTest, '13 de desembre de 2020');
+    }
+
+    /** @test */
+    public function can_get_formatted_published_at_date_when_not_published()
+    {
+        // 1 Preparació
+        // TODO CODE SMELL
+        $video = Video::create([
+            'title' => 'Ubuntu 101',
+            'description' => '# Here description',
+            'url' => 'https://youtu.be/w8j07_DBl_I',
+            'published_at' => null,
+            'previous' => null,
+            'next' => null,
+            'series_id' => 1
+        ]);
+
+        // 2 Execució WISHFUL PROGRAMMING
+        $dateToTest = $video->formatted_published_at;
+
+        // 3 comprovació / assert
+        $this->assertEquals($dateToTest, '');
     }
 }
