89c0c5423458a38aec0068bef083c61412565cb3
sergicasado <sergicasado1@iesebre.com>
2022-04-20 17:24:42 +0200

    wip

--- app/Listeners/SendVideoCreatedNotification.php
+++ app/Listeners/SendVideoCreatedNotification.php
@@ -4,19 +4,23 @@ namespace App\Listeners;
 
 use App\Events\VideoCreated;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Notification;
+use Tests\Unit\SendVideoCreatedNotificationTest;
 
-class SendVideoCreatedNotification
+class SendVideoCreatedNotification implements
+ShouldQueue
 {
-    /**
-     * Handle the event.
-     *
-     * @param  VideoCreated  $event
-     * @return void
-     */
+    public static function testedBy()
+    {
+        return
+        SendVideoCreatedNotificationTest::class;
+    }
+
     public function handle(VideoCreated $event)
     {
-        Notification::route('mail', config('casteaching.admins'))->notify(new \App\Notifications\VideoCreated($event->video));
+        Notification::route('mail',
+        config('casteaching.admins'))->notify(new \App\Notifications\VideoCreated($event->video));
     }
 }
+
+
